065caf3088e8e3ef0adc4c95dd1ba2c9b5ae10bd
chintan <chintan_er@yahoo.com>
2021-02-20 10:10:44 -0500

    Added new test

--- Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -12,5 +12,11 @@ public class AppiumTest {
 	public void NewTest() {
 		System.out.println("New test");
 	}
+	
+	@Test
+	public void NewTestFromChintan() {
+		System.out.println("New test From Chin2");
+	}
+
 
 }
